1ce89a33e9e2b8851c7af081e24ebe2482435c85
paulo94kdh@gmail.com <youcode21youcode@outlook.fr>
2021-11-11 11:32:10 +0100

    calculer le rayon

---
+++ Calcule Le rayon/calculer_le_Rayon.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    //circonference d'un cercle
+    const p=3.14;
+    int r;
+    int CR;
+    
+    
+    printf("entrer la rayon: \n");
+    scanf("%d",&r);
+    
+    
+    CR=2*p*r;
+
+    printf("les resultas Final est %d :\n",CR);
+    
+    return 0;
+}
