24dd1059c0630b997a4fea700536ffbd6613aeb5
paulo94kdh@gmail.com <youcode21youcode@outlook.fr>
2021-11-11 14:18:54 +0100

    conversion to bin and hexad

---
+++ convert to hexa and decimal/Convert_To_Hex_Dec.C
@@ -0,0 +1,18 @@
+
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int Number;
+
+    printf("entrer nUmbres: \n");
+    scanf("%d",&Number);
+   
+    
+    printf("la conversion de %d to octal est : %o\n",Number,Number);
+        printf("la conversion de %d to hexadecimal est : %x\n",Number,Number);
+
+    
+    return 0;
+}
