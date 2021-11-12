00e7ee2312ed376013e04738ef8f8a90687b5e6c
paulo94kdh@gmail.com <youcode21youcode@outlook.fr>
2021-11-12 10:34:49 +0100

    IsCaractere And Islower and Upper

---
+++ Chalenege_8_Condition/caractere_et_miniscule.c
@@ -0,0 +1,39 @@
+        #include <stdio.h>
+        #include <math.h>
+        #include <string.h>
+        #include <ctype.h>
+
+        
+        
+        int main()
+        {
+           
+           //la mention obtenir par un eleve
+           
+           char Ca;
+           printf("Entrer un Caractere :\n");
+           scanf("%c",&Ca);
+           
+           int x=isdigit(Ca);
+           int y=isupper(Ca);
+           if(x==0)
+           {
+               printf("Bien ,Vous aver entrer un caractere \n");
+               
+               if(y==1)
+               {
+                   printf("le caractere est en Majuscule \n");
+               
+                   
+               }else{
+                 printf("le caractere est en Minuscile \n");
+
+               }
+           }else{
+               
+               printf("please ,Entrer Un caractere");
+           }
+           
+       
+            return 0;
+    }
